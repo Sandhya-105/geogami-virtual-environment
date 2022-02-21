f5b2f882420b54656b638b017ed644535c4805d8
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2022-02-21 12:22:43 +0100

    #13: bug fixed

--- origami-VR-world/Assets/Scripts/TextTransfer.cs
+++ origami-VR-world/Assets/Scripts/TextTransfer.cs
@@ -18,37 +18,35 @@ public class TextTransfer : MonoBehaviour
     //public Toggle world1_cbox;
     void Start()
     {
-        //Debug.Log("start fomr text trasnfer!!");
+        //Debug.Log("on Start!!");
         socket.On("checkRoomExistance", GetRoomStatus);
     }
 
     public void StoreCode()
     {
+        //Debug.Log("on StoreCode!!");
         socket.Emit("checkRoomExistance", new JSONObject("{\"gameCode\": \"" + gameCode_tbox.text.ToString() + "\"}"));
     }
 
     // Check if room already created 
     public void GetRoomStatus(SocketIOEvent e)
     {
-        //Debug.Log("RoomStatus:b=  "+ System.Convert.ToBoolean(e.data["RoomStatus"].ToString()));
+        //Debug.Log("roomCode:b=  "+  (e.data["roomCode"].ToString()).GetType() + ", Val: "+e.data["roomCode"].ToString());
+        
+        // check user (this will prevent other users who are in main scene to join game with currrent room)
+        if(string.Equals(e.data["roomCode"].ToString(),("\""+gameCode_tbox.text.ToString()+"\""))){
+            if (System.Convert.ToBoolean(e.data["roomStatus"].ToString()))
+            { // if true allow user to join the game
+                gameDetails["gameCode"] = gameCode_tbox.text.ToString();
+                gameDetails["playerName"] = playerName_tbox.text.ToString();
+                gameDetails["vr_world_1_status"] = e.data["roomVRWorldType"].ToString();
 
-        if (System.Convert.ToBoolean(e.data["RoomStatus"].ToString()))
-        { // if true allow user to join the game
-            gameDetails["gameCode"] = gameCode_tbox.text.ToString();
-            gameDetails["playerName"] = playerName_tbox.text.ToString();
-            gameDetails["vr_world_1_status"] = e.data["roomVRWorldType"].ToString();
-            //gameDetails["vr_world_1_status"] = world1_cbox.isOn.ToString();
-
-            //Debug.Log("roomVRWorldType: "+ e.data["roomVRWorldType"].ToString());
-            //Debug.Log("gameCode_tbox: "+ gameDetails["gameCode"]);
-            //Debug.Log("Name_tbox: "+ gameDetails["gameCode"]);
-            // Move to game play scene
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        else
-        { // if false, show a notificatoin that game is not yet created
-            //Debug.Log("Room doesn't exist!!!, RoomStatus: "+e.data["RoomStatus"]);
-            SSTools.ShowMessage("Game code is not correct!", SSTools.Position.bottom, SSTools.Time.twoSecond);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {   // if false, show a notificatoin that game code is not yet created
+                SSTools.ShowMessage("Game code is not correct!", SSTools.Position.bottom, SSTools.Time.twoSecond);
+            }
         }
 
     }
